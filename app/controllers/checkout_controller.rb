7c07bbf55b9f64bec82832d8cda445fd911f3c11
Benshalom <benshalom.kariuki@kopokopo.com>
2022-02-24 07:07:39 +0000

    add stripe checkout session

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,19 +5,19 @@ class CheckoutController < ApplicationController
       @session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
       line_items: [{
-      name: product.name,
-      amount: product.price,
-      currency: "usd", 
-      quantity: 1
-      
-  }],
-  mode: 'payment',
-  success_url: root_url,
-  cancel_url: root_url,
+          name: product.name,
+          amount: product.price,
+          currency: "usd", 
+          quantity: 1
+      }],
+         mode: 'payment',
+         success_url: root_url,
+         cancel_url: root_url,
 })
 respond_to do |format|
     format.js
-end 
     end 
     
+  end 
+
 end 
